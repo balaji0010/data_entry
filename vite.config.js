d54c6c9206fd155e9e146c9077b211889069dcba
balaji <uibalaji29@gmail.com>
2025-07-20 00:17:44 +0530

    setup

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue'; // <-- import Vue plugin
+import path from 'path';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/scss/app.scss', 'resources/js/app.js'],
+            refresh: true,
+        }),
+       vue(), // <-- add Vue plugin here
+    ],
+    resolve: {
+        alias: {
+            // Important: use Vue with compiler support
+            vue: 'vue/dist/vue.esm-bundler.js',
+            '@': path.resolve(__dirname, 'resources/js'),
+        },
+    },
+});
